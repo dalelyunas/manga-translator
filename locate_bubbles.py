4fc02832c073e5b98081444adbad43aca00bd8e4
Derick Anderson <eanaros@gmail.com>
2017-09-30 14:41:07 -0400

    IT WORKS

--- locate_bubbles.py
+++ locate_bubbles.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python
 
 from PIL import Image
+from ocr import Blurb
 
+import sys
 import numpy as np
+import dill as pickle
 import cv2
 
 
 import pytesseract
 
-if __name__ == '__main__':
-  img = cv2.imread("img/opm-1.jpg")
 
+if __name__ == '__main__':
+  img = cv2.imread(sys.argv[1])
+  get_blurbs(img)
+
+
+def get_blurbs(img):
   img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   img_gray = cv2.bitwise_not(cv2.adaptiveThreshold(img_gray, 255, cv2.THRESH_BINARY, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 75, 10))
 
@@ -34,17 +41,30 @@ if __name__ == '__main__':
   im2, contours2, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
   final_mask = cv2.cvtColor(np.zeros_like(img), cv2.COLOR_BGR2GRAY)
+
+  blurbs = []
   for cnt in contours2:
     area = cv2.contourArea(cnt)
     if area > 1000 and area < ((height / 4) * (width / 4)):
       draw_mask = cv2.cvtColor(np.zeros_like(img), cv2.COLOR_BGR2GRAY)
       approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+      pickle.dump(approx, open("approx.pkl", mode="w"))
       cv2.fillPoly(draw_mask, [approx], (255,0,0))
       cv2.fillPoly(final_mask, [approx], (255,0,0))
       image = cv2.bitwise_and(draw_mask, cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+      y = approx[:, 0, 1].min()
+      h = approx[:, 0, 1].max() - y
+      x = approx[:, 0, 0].min()
+      w = approx[:, 0, 0].max() - x
+      image = image[y:y+h, x:x+w]
       pil_image = Image.fromarray(image)
-      pil_image.show()
-      text = pytesseract.image_to_string(pil_image, lang="jpn_vert")
-      print "Ateempt: " + text
+#      pil_image.show()
+      text = pytesseract.image_to_string(pil_image, lang="jpn_vert", config="")
+      if text:
+        blurb = Blurb(x, y, w, h, text)
+        blurbs.append(blurb)
+      print "Attempt: " + text
 
-Image.fromarray(final_mask).show()
+  return blurbs
+
+
