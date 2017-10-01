cb90fd72e199efd9e22e50b285009e7dee729f41
Derick Anderson <eanaros@gmail.com>
2017-10-01 10:26:06 -0400

    Specify Japanese

--- translate.js
+++ translate.js
@@ -2,7 +2,7 @@
 
 const translate = require('google-translate-api');
 
-translate(process.argv[2], {to: 'en'}).then(res => {
+translate(process.argv[2], {from: 'ja', to: 'en'}).then(res => {
     console.log(res.text);
 }).catch(err => {
     console.error(err);
--- translate.py
+++ translate.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 
 class Blurb(object):
